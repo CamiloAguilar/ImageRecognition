9a7cdab64471555026822a599272ab4ac2f8fe72
DESKTOP-2LPI0J9\Camilo <aguilarcamiloa@gmail.com>
2018-07-31 18:52:55 -0500

    ObjRec

---
+++ 02_ObjectDetection/01_ObjRecognition.py
@@ -0,0 +1,12 @@
+#********************************
+##        SECCIÓN 2
+#********************************
+
+## Algoritmos básicos
+## Características y descriptores
+## Reconocimiento a partir de pareo de características
+## Construcción de una aplicación
+
+
+
+
